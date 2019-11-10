5e8163e33d2e6281f6b00539b593b32df5ac10cf
Ricardo Velhinho <36649075+RVelhinho@users.noreply.github.com>
2019-11-10 02:55:04 +0000

    Updates

---
+++ precache-manifest.74ec853d605d3d459a482c4660345d01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26346bf3681b6916a00eb41bcbedda2e",
+    "url": "/my-page/index.html"
+  },
+  {
+    "revision": "8d00738a6105fb147d87",
+    "url": "/my-page/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b2c8e5aaf9ff536b17c1",
+    "url": "/my-page/static/js/2.d8c4dfee.chunk.js"
+  },
+  {
+    "revision": "8d00738a6105fb147d87",
+    "url": "/my-page/static/js/main.b061c2c0.chunk.js"
+  },
+  {
+    "revision": "a58745f62b59be526e3c",
+    "url": "/my-page/static/js/runtime-main.99b3c895.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-page/static/media/logo.25bf045c.svg"
+  }
+]);
