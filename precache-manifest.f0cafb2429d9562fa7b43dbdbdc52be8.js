d67eb1e3bb89a4abfd8bf113d6ab5da50f55ca5e
Ricardo Velhinho <36649075+RVelhinho@users.noreply.github.com>
2019-12-20 21:38:36 +0000

    Updates

---
+++ precache-manifest.f0cafb2429d9562fa7b43dbdbdc52be8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be14c613babc07bd7aa7e0720ec3201",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f96c611609e7adaec2b",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "c8e65743621c0450885f",
+    "url": "/static/css/main.3fcadb1b.chunk.css"
+  },
+  {
+    "revision": "2f96c611609e7adaec2b",
+    "url": "/static/js/2.7a6516bb.chunk.js"
+  },
+  {
+    "revision": "c8e65743621c0450885f",
+    "url": "/static/js/main.945af8c0.chunk.js"
+  },
+  {
+    "revision": "0453e942e016472495a4",
+    "url": "/static/js/runtime-main.d247ced4.js"
+  },
+  {
+    "revision": "ffea743704acddbd7c7582f9c92641a6",
+    "url": "/static/media/eu.ffea7437.jpg"
+  },
+  {
+    "revision": "01ad16046d712cda0b4911e275ecf1a4",
+    "url": "/static/media/ist.01ad1604.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-page/precache-manifest.49a84efc55ed48ab221099cd2fd7511f.js"
+  "/precache-manifest.f0cafb2429d9562fa7b43dbdbdc52be8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-page/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
