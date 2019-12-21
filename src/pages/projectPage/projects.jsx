c5cc003a6acc9cd08e05f5c16c0737d28eff3f91
Ricardo Velhinho <36649075+RVelhinho@users.noreply.github.com>
2019-12-21 21:37:58 +0000

    fixed m

--- src/pages/projectPage/projects.jsx
+++ src/pages/projectPage/projects.jsx
@@ -13,12 +13,12 @@ class Projects extends Component {
                     </div>
                 </div>
                 <hr className='w-75 my-4'/>
-                <div className='row w-100 h-75 justify-content-center mt-5'>
-                    <div className='col-8 col-sm-5 col-md-4 col-lg-4 text-center' align='center'>
+                <div className='row w-100 h-75 justify-content-center mt-5 mx-0'>
+                    <div className='col-12 col-sm-5 col-md-4 col-lg-4 text-center' align='center'>
                         <a href='https://github.com/ggalambas/1920-MEIC-VI'><img src={Youtube} className='img-fluid border border-dark rounded'></img></a>
                         <p className='display-5 text-dark mt-3'>Youtube Trends Data Visualization (using D3 library)</p>
                     </div>
-                    <div className='col-8 col-sm-5 col-md-4 col-lg-4 text-center' align='center'>
+                    <div className='col-12 col-sm-5 col-md-4 col-lg-4 text-center' align='center'>
                         <a href='https://github.com/TheMrKiko/CCU-APP_S1920'><img src={UnderWay} className='img-fluid border border-dark rounded'></img></a>
                         <p className='display-5 text-dark mt-3'>Underway Mobile App - Metro services (using React Native + Expo Framework)</p>
                     </div>
