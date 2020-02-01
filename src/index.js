321b9a9812a712f7759711188a7f0e48df9e5873
Ricardo Velhinho <36649075+RVelhinho@users.noreply.github.com>
2020-02-01 18:32:01 +0000

    hmm

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-<HashRouter>
+<BrowserRouter>
     <App />
-</HashRouter>
+</BrowserRouter>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
